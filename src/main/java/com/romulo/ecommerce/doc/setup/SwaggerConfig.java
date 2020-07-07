6db253c0321faa8473d87e5ec499633b47a607dd
Rômulo Franco <romulojosefranco@gmail.com>
2020-07-07 14:40:09 -0300

    Swagger api doc has created

--- src/main/java/com/romulo/ecommerce/doc/setup/SwaggerConfig.java
+++ src/main/java/com/romulo/ecommerce/doc/setup/SwaggerConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,21 +17,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
-  @Bean
-  public Docket api() {
-    return new Docket(DocumentationType.SPRING_WEB )
-      .select()
-      .apis(RequestHandlerSelectors.basePackage("com.romulo.ecommerce.api"))              
-//      .paths(PathSelectors.any())                          
-      .build();  
-  }
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SPRING_WEB).select()
+				.apis(RequestHandlerSelectors.basePackage("com.romulo.ecommerce.api"))                       
+				.build().apiInfo(metaData());
+	}
 
-  @Override
-  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("swagger-ui.html")
-      .addResourceLocations("classpath:/META-INF/resources/");
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder().title("Spring Boot REST API").description("Spring Boot REST API for an eCommerce Sample")
+				.version("1.0.0").license("GNU License").licenseUrl("https://www.gnu.org/licenses/licenses.pt-br.html")
+				.contact(new Contact("Rômulo Franco", "https://github.com/romulofranco", "romulojosefranco@gmail.com"))
+				.build();
+	}
 
-    registry.addResourceHandler("/webjars/**")
-    .addResourceLocations("classpath:/META-INF/resources/webjars/");
-  }
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 }
